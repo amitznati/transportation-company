da2dc32cb98bbd52c839dcd641904893c10d3863
AmitZnati <amitznati@gmail.com>
2018-05-02 16:53:36 +0200

    Eureka

--- db-service/src/main/java/com/trans/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/trans/dbservice/DbServiceApplication.java
@@ -2,8 +2,12 @@ package com.trans.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = "com.trans.dbservice.repositories")
 @SpringBootApplication
 public class DbServiceApplication {
